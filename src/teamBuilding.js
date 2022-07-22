c0502a301332de86668bf099aa6ac0d5864c5f22
Matt F <fiaschettima@gmail.com>
2022-07-21 21:55:00 -0400

    added personal stylesheet for color changes/ slightly changed card layout

--- src/teamBuilding.js
+++ src/teamBuilding.js
@@ -1,65 +1,110 @@
-
+// cards are tabbed so far to keep good indentation when HTML is generated
 const managerCard = (person) => {
     return `
-        <div class ="col-12 col-sm-6 col-md-4 g-4 text-center">
-            <div class="card">
-                <div class="card-header">
-                    <h3>${person.getName()}</h3>
+            <div class ="col-12 col-sm-6 col-md-4 g-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>${person.getName()}</h3>
+                    </div>
+                    <div class="card-subtitle">
+                        <h4><i class="fa-solid fa-user-tie"></i>${person.getRole()}</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group ">
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                                <div class="fw-bold">ID:</div>
+                                ${person.getId()}
+                            </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">Email</div>
+                                <a href="mailto:${person.getEmail()}" target="_blank">${person.getEmail()}</a>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">Office Number:</div>
+                                ${person.getOfficeNum()}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="card-subtitle">
-                    <h4>${person.getRole()}</h4>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">${person.getId()}</li>
-                        <li class="list-group-item">${person.getEmail()}</li>
-                        <li class="list-group-item">${person.getOfficeNum()}</li>
-                    </ul>
-                </div>
-            </div>
-        </div>    
+            </div>    
     `
 }
 const engineerCard = (person) => {
     return `
-        <div class ="col-12 col-sm-6 col-md-4 g-4 text-center">
-            <div class="card">
-                <div class="card-header">
-                    <h3>${person.getName()}</h3>
-                </div>
-                <div class="card-subtitle">
-                    <h4>${person.getRole()}</h4>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">${person.getId()}</li>
-                        <li class="list-group-item">${person.getEmail()}</li>
-                        <li class="list-group-item">${person.getGithub()}</li>
-                    </ul>
+                <div class ="col-12 col-sm-6 col-md-4 g-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>${person.getName()}</h3>
+                    </div>
+                    <div class="card-subtitle">
+                        <h4><i class="fa-solid fa-user-gear"></i>${person.getRole()}</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group ">
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                                <div class="fw-bold">ID:</div>
+                                ${person.getId()}
+                            </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">Email</div>
+                                <a href="mailto:${person.getEmail()}" target="_blank">${person.getEmail()}</a>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">GitHub:</div>
+                                ${person.getGithub()}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
     `
 }
 const internCard = (person)=>{
     return `
-        <div class ="col-12 col-sm-6 col-md-4 g-4 text-center">
-            <div class="card">
-                <div class="card-header">
-                    <h3>${person.getName()}</h3>
-                </div>
-                <div class="card-subtitle">
-                    <h4>${person.getRole()}</h4>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">${person.getId()}</li>
-                        <li class="list-group-item">${person.getEmail()}</li>
-                        <li class="list-group-item">${person.getSchool()}</li>
-                    </ul>
+                <div class ="col-12 col-sm-6 col-md-4 g-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>${person.getName()}</h3>
+                    </div>
+                    <div class="card-subtitle">
+                        <h4><i class="fa-solid fa-user-graduate"></i>${person.getRole()}</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group ">
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                                <div class="fw-bold">ID:</div>
+                                ${person.getId()}
+                            </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">Email</div>
+                                <a href="mailto:${person.getEmail()}" target="_blank">${person.getEmail()}</a>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">School:</div>
+                                ${person.getSchool()}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
     `
 }
 // for loop here to iterate through people array and make cards for each
@@ -88,6 +133,8 @@ const  teamBuilding = (people) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/21ead0173a.js" crossorigin="anonymous"></script>
+    <link href="./style.css" rel="stylesheet">
     <title>Team List</title>
 </head>
 <body>
